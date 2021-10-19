b53db6ade3927e937eb015fcac11c026309c37f8
john cheney <john.cheney@student.centralia.edu>
2021-10-18 20:45:40 -0700

    Add project files.

---
+++ Cheney_LinkedList/Linked_list.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheney_LinkedList
+{
+    class Linked_list
+    {
+        private Node Current;
+        public Node Add(Node node, Node NewNode)
+        {
+            if(node == null)
+            {
+                return NewNode;
+            }
+
+            Current = node;
+            while (Current != null)
+            {
+                if (Current.Next == null)
+                {
+                    Current.Next = NewNode;
+                    break;
+                }
+                else
+                {
+                    Current = Current.Next;
+                }
+            }
+            return node;
+        }
+        public Node Contains(Node node ,string Value)
+        {
+            Current = node;
+            while (Current != null)
+            {
+                if (Current.Datas != Value)
+                {
+                    if (Current.Next != null)
+                    {
+                        Current = Current.Next;
+                    }
+                    else if(Current.Next == null)
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return Current;
+                }
+            }
+            return node;
+        }
+        public void Remove(Node node, string Value, out bool Isitnull)
+        {
+            Isitnull = false;
+            Current = node;
+            if (Current == null)
+            {
+                Isitnull = true;
+                return;
+            }
+            if (Current.Datas == Value)
+            {
+                Current = Current.Next;
+                return;
+            }
+
+             
+            while (Current.Next != null)
+            {
+                if (Current.Next.Datas == Value)
+                {
+                    Current.Next = Current.Next.Next;
+                    return;
+                }
+
+                Current = Current.Next;
+            }
+            if(Current.Next == null)
+            {
+                Isitnull = true;
+                return;
+            }
+        }
+        public void PrintAll(Node Current)
+        {
+
+            while (Current != null)
+            {
+                Console.WriteLine(Current.Datas);
+                if (Current.Next != null)
+                    Current = Current.Next;
+                else
+                    break;
+            }
+            return;
+        }
+    }
+}
---
+++ Cheney_LinkedList/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Cheney_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node Head = null;
+            Linked_list LL = new Linked_list();
+            ConsoleKeyInfo entry;
+            Console.WriteLine("Welcome. Please select a number to get started.");
+            bool Escape = false;
+            do
+            {
+                Console.WriteLine("1. Add item");
+                Console.WriteLine("2. Search item");
+                Console.WriteLine("3. Remove item");
+                Console.WriteLine("4. Print list");
+                Console.WriteLine("5. Exit");
+                entry = Console.ReadKey();
+                Console.Clear();
+                if (entry.Key == ConsoleKey.NumPad1 || entry.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("Enter word to add to list");
+                    string Nodeitem = Console.ReadLine();
+                    Head = LL.Add(Head, new Node(Nodeitem));
+                    Console.WriteLine("Item added to Node. Press any key to return");
+                    Console.ReadKey();
+                    Console.Clear();
+                    
+                }
+                if (entry.Key == ConsoleKey.NumPad2 || entry.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("Enter word to search in list");
+                    string Nodeitem = Console.ReadLine();
+                    Node test= LL.Contains(Head, Nodeitem);
+                    if(test != null)
+                    {
+                        Console.WriteLine("Item is listed in Node " + test.Datas);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item does not exist in nodes");
+                    }
+                    Console.WriteLine("Press any key to return.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (entry.Key == ConsoleKey.NumPad3 || entry.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("Enter word to search an item to delete from list");
+                    string Nodeitem = Console.ReadLine();
+                    LL.Remove(Head, Nodeitem, out bool isitnull);
+                    if (isitnull != true)
+                    {
+                        Console.WriteLine("Item in Node removed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item does not exist in nodes");
+                    }
+                    Console.WriteLine("Press any key to return.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (entry.Key == ConsoleKey.NumPad4 || entry.Key == ConsoleKey.D4)
+                {
+                    Console.WriteLine("Node items:");
+                    LL.PrintAll(Head);
+                    Console.WriteLine("Press any key to return.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (entry.Key == ConsoleKey.NumPad5 || entry.Key == ConsoleKey.D5)
+                {
+                    Console.WriteLine("Press any key to terminate application");
+                    Escape = true;
+                    Console.ReadKey();
+                }
+
+
+
+            } while (Escape != true);
+                    
+        }
+    }
+}
